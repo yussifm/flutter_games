562f42cf79fe52bdba2f22dac9bccdd36fe4baf8
yussifm <yussifm13@gmail.com>
2022-08-14 16:34:12 +0000

    f

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Games plugins Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Games plugins Demo'),
     );
   }
 }
